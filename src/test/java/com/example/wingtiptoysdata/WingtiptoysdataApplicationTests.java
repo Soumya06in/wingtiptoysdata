e2adc119b67a8964f1c8d50673bb99a970f14879
Soumya06in <soumyabasu06in@gmail.com>
2020-09-24 10:57:15 +0530

    first commit

---
+++ src/test/java/com/example/wingtiptoysdata/WingtiptoysdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wingtiptoysdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingtiptoysdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
